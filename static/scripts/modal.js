ea573fb4aa2b1fc81ce9b6928c34d48c62890b76
nadiadek <nadiasilva@ug.uchile.cl>
2021-05-29 17:55:58 -0400

    Avance Tarea

--- static/scripts/modal.js
+++
@@ -1,9 +0,0 @@
-function cerrar(i= 0) {
-    let modal = document.getElementById("modal1");
-    let div = document.getElementById("imagenes-modal");
-    let img = document.getElementById("imagenModal");
-    if (i === 0) {
-        div.removeChild(img);
-    }
-    modal.classList.remove("is-visible");
-}
